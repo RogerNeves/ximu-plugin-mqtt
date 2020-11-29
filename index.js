991c2c932b3267cae2e0539b8deb8ae030d630f7
Roger Neves <rnevesmattos@gmail.com>
2020-11-29 10:51:56 -0300

    ultimo update antes da subida pra testes.

---
+++ index.js
@@ -0,0 +1,56 @@
+const mqtt = require('mqtt')
+const axios = require("axios")
+
+let connections = []
+let responses = []
+
+var getConnections = async () => {
+  await axios.get("http://localhost:3001/mqtt")
+    .then(function (res) {
+      if (connections) 
+        connections.forEach(connection => connection.end() )
+      
+      connections = []
+      responses = res.data
+      responses.forEach(async (response, index) => {
+        let connection = mqtt.connect(`mqtt://${response.url}:${response.port}`,
+          {
+            clientId: 'XimuIot',
+            username: response.username,
+            password: response.password,
+            clean: true
+            
+          })
+        connection.topic = response.topic
+
+        connection.on( 'connect', () =>{
+          console.log(response.topic)
+          connection.subscribe(response.topic)
+        } )
+        connection.on('message', sendMeansurements)
+        connections.push(connection)
+      })      
+    }).catch(function (error) {
+      console.log(error)
+    });
+}
+
+
+const sendMeansurements = function (topic, message, packet) {
+  const object = JSON.parse(message);
+  const {authorization} = object
+  console.log(object)
+  axios.post('http://localhost:3001/meansurements',object ,{
+    headers: {
+      authorization
+    }
+  })
+    
+}
+setInterval(getConnections, 10000)
+
+
+
+
+
+
